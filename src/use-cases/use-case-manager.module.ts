948cc0cafa09167bfec3cfd640f6b8e76ae293ad
thai.nguyenvan.clv <thai.nguyenvan@cyberlogitec.com>
2024-03-28 11:04:33 +0700

    feat(28032024): init grpc and sample grpc service

--- src/use-cases/use-case-manager.module.ts
+++ src/use-cases/use-case-manager.module.ts
@@ -1,7 +1,7 @@
-import { Module } from '@nestjs/common';
-import { AddSampleUseCase, GetSampleUseCase } from './sample';
-@Module({
-  providers: [GetSampleUseCase, AddSampleUseCase],
-  exports: [GetSampleUseCase, AddSampleUseCase],
-})
-export class UseCaseManagerModule {}
+// import { Module } from '@nestjs/common';
+// import { AddSampleUseCase, GetSampleUseCase } from './sample';
+// @Module({
+//   providers: [GetSampleUseCase, AddSampleUseCase],
+//   exports: [GetSampleUseCase, AddSampleUseCase],
+// })
+// export class UseCaseManagerModule {}
