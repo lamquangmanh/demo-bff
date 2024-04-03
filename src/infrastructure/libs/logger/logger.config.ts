97e973cb835d4a7d356751ea0d424f8bd2cd61fd
thai.nguyenvan.clv <thai.nguyenvan@cyberlogitec.com>
2024-04-03 13:50:14 +0700

    refactor(03242024): update format message logger

--- src/infrastructure/libs/logger/logger.config.ts
+++ src/infrastructure/libs/logger/logger.config.ts
@@ -1,7 +1,27 @@
 import { ConsoleLogger } from '@nestjs/common';
 
 export class Logger extends ConsoleLogger {
-  error(message: any, stack?: string, context?: string) {
-    super.error(message, stack, context);
+  error(message: any, userId?: string, stack?: string, context?: string) {
+    super.error(`${message} [${userId}]`, stack, context);
+  }
+
+  log(message: any, userId?: string, context?: string) {
+    super.error(`${message} [${userId}]`, context);
+  }
+
+  warn(message: any, userId?: string, context?: string) {
+    super.error(`${message} [${userId}]`, context);
+  }
+
+  debug(message: any, userId?: string, context?: string) {
+    super.error(`${message} [${userId}]`, context);
+  }
+
+  verbose(message: any, userId?: string, context?: string) {
+    super.error(`${message} [${userId}]`, context);
+  }
+
+  fatal(message: any, userId?: string, context?: string) {
+    super.error(`${message} [${userId}]`, context);
   }
 }
