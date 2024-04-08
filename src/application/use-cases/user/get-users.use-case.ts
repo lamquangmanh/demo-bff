a586b91537825ac92ed7299156ed097ae24a031e
Phuoc <thienphuoc19032001@gmail.com>
2024-04-08 14:12:02 +0700

    fix: fix get user return empty users

--- src/application/use-cases/user/get-users.use-case.ts
+++ src/application/use-cases/user/get-users.use-case.ts
@@ -9,9 +9,10 @@ export class GetUsersUseCase implements GetUsersUseCaseAbstract {
   constructor(private grpcService: GrpcContextAbstract) {}
 
   async execute(filter: GetUsersRequest): Promise<GetUsersResponse> {
-    console.log('zo');
     const res = await this.grpcService.userService.listUser(filter);
-    console.log({ res });
+    if (!res?.data) {
+      res.data = [];
+    }
     return res;
   }
 }
